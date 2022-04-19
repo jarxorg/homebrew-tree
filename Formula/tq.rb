1bcd4f6b8145c1abc2484140322802a44dead9a3
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-19 12:37:07 +0900

    Brew formula update for tree version v0.6.1

--- Formula/tq.rb
+++ Formula/tq.rb
@@ -5,20 +5,20 @@
 class Tq < Formula
   desc "tq is a portable command-line JSON/YAML processor."
   homepage "https://github.com/jarxorg/tree#tq"
-  version "0.6.0"
+  version "0.6.1"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/jarxorg/tree/releases/download/v0.6.0/tree_0.6.0_Darwin_x86_64.tar.gz"
-      sha256 "f41bf354b004d874a942c5bebb8c38fc6ccb513236eb5794393c76269befe57d"
+    if Hardware::CPU.arm?
+      url "https://github.com/jarxorg/tree/releases/download/v0.6.1/tree_0.6.1_Darwin_arm64.tar.gz"
+      sha256 "681b0b2330f3147322c08345d043130cb1c2eee05ca688f86a1b6fc6c09db37d"
 
       def install
         bin.install "tq"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/jarxorg/tree/releases/download/v0.6.0/tree_0.6.0_Darwin_arm64.tar.gz"
-      sha256 "1f08b91156dc12e7775b61b4c685d036bd16c827cfbf6f410540604f16cbbdba"
+    if Hardware::CPU.intel?
+      url "https://github.com/jarxorg/tree/releases/download/v0.6.1/tree_0.6.1_Darwin_x86_64.tar.gz"
+      sha256 "10c15e8a6ea48ee706f2e4a46ac6659b1f00744354d3f64e16ee10835c275dc4"
 
       def install
         bin.install "tq"
@@ -27,17 +27,17 @@ class Tq < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jarxorg/tree/releases/download/v0.6.0/tree_0.6.0_Linux_arm64.tar.gz"
-      sha256 "ae9d6c30e08c290cebedf0348fb92b6d6c3e8e98883efd613f2cbdaf2bd747e9"
+    if Hardware::CPU.intel?
+      url "https://github.com/jarxorg/tree/releases/download/v0.6.1/tree_0.6.1_Linux_x86_64.tar.gz"
+      sha256 "67dea66b24bca3952435d383ea8c2574a53ba9993ad7f3a0cf470cc2ea42eb32"
 
       def install
         bin.install "tq"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/jarxorg/tree/releases/download/v0.6.0/tree_0.6.0_Linux_x86_64.tar.gz"
-      sha256 "f8eb2b6feea2c929cef423f0df935dcc7f89cbb41c566f3c67fc90655afe9b18"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/jarxorg/tree/releases/download/v0.6.1/tree_0.6.1_Linux_arm64.tar.gz"
+      sha256 "1ce1dc137c88b2bf4462abbdca5b8a03d6325a8258572c64e7deb1a29c19fb27"
 
       def install
         bin.install "tq"
